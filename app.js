2329decaa0856585a899e8b6020c53462d3ec32c
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-24 17:16:34 +0300

    Linked data with frontend

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const listdata = require("./listdata");
 const app = express();
+app.use(cors());
+
+app.get("/list", (req, res) => {
+  res.json(listdata);
+});
 
 app.listen(8000, () => {
   console.log("Running on port 8000");
---
+++ listdata.js
@@ -0,0 +1,13 @@
+let listdata = [
+  { name: "Running", priority: "high", id: 1 },
+  { name: "Eating", priority: "miduim", id: 2 },
+  { name: "Read my new books", priority: "low", id: 3 },
+  {
+    name: "Purchase some Brew mi and alot of peanut pro",
+
+    priority: "high",
+    id: 4,
+  },
+];
+
+module.exports = listdata;
